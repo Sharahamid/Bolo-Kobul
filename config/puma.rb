0d755c2d934e80ccf37ebeb0fd438360a3f66230
Shara Hamid <shara@bolokobul.com>
2026-02-25 03:51:44 +0600

    Resolve conflict: use golden X icon with updated classes

--- config/puma.rb
+++ config/puma.rb
@@ -1,5 +1,3 @@
-# Puma configuration file
-
 # Bind Puma to a Unix socket for Nginx
 bind "unix:///home/ubuntu/apps/bolokobul/shared/tmp/sockets/bolokobul-puma.sock"
 
@@ -10,10 +8,18 @@ state_path "/home/ubuntu/apps/bolokobul/shared/tmp/pids/puma.state"
 # Threads settings
 threads 5, 5
 
+# Workers for multi-core
+workers 2
+preload_app!
+
 # Environment
 environment ENV.fetch("RAILS_ENV") { "production" }
 
+# Daemonize in production
+daemonize true if ENV.fetch("RAILS_ENV") { "production" } == "production"
+
+# Logging
+stdout_redirect "/home/ubuntu/apps/bolokobul/shared/log/puma.stdout.log", "/home/ubuntu/apps/bolokobul/shared/log/puma.stderr.log", true
+
 # Allow puma to be restarted by `rails restart`
 plugin :tmp_restart
-
-
